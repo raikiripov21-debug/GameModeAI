cc98811cb97faa580e1f5bd4d055b482ba218257
raikiripov21-debug <raikiripov21@gmail.com>
2026-04-18 06:29:47 -0300

    restore: volver a estructura original A06 (e449109)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -14,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "GameModeAI-A06"
+rootProject.name = "GameModeAI"
 include(":app")
